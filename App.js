7c456fa4ddeec1a3de5d00a621254d7f8037a8d0
DavitVosk <dvoskanyan2@gmail.com>
2019-11-08 21:00:17 +0400

    Include react navigation

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from "react";
-import Home from "./src/screens/home";
-import { store } from "./src/redux";
 import { Provider } from "react-redux";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Home from "./src/screens/home";
+import Quiz from "./src/screens/quiz";
+import Results from "./src/screens/results";
+import { store } from "./src/redux";
+
+const RootStack = createStackNavigator({
+  Home: {
+    screen: Home,
+  },
+  Quiz: {
+    screen: Quiz,
+  },
+  Results: {
+    screen: Results,
+  },
+});
+
+let Navigation = createAppContainer(RootStack);
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Home />
+        <Navigation />
       </Provider>
     );
   }
