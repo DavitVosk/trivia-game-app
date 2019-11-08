c0af8420b7d9fd99f6d6ab4bd6a58a86be5cfaf9
DavitVosk <dvoskanyan2@gmail.com>
2019-11-08 19:19:12 +0400

    App.js: fix some files direction

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Home from "./screens/home";
-import { store } from "./redux";
+import Home from "./src/screens/home";
+import { store } from "./src/redux";
 import { Provider } from "react-redux";
 
 export default class App extends React.Component {
