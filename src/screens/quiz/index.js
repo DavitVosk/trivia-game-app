1ce0bc9beb81b5f79d27b647ebaae955a86c2bb1
DavitVosk <dvoskanyan2@gmail.com>
2019-11-12 19:35:46 +0400

    Quiz screen: code refactoring

--- src/screens/quiz/index.js
+++ src/screens/quiz/index.js
@@ -3,18 +3,18 @@ import { Text, View, TouchableOpacity, SafeAreaView, ImageBackground } from 'rea
 import { connect } from 'react-redux'
 
 import { fetchQuestions } from '../../redux/actions/FetchQuestions';
-import CardsList  from './CardsList';
-import Loading  from './Loading';
+import CardsList from './CardsList';
+import Loading from './Loading';
 import LinearGradientButton from '../../../CommonComponents/LinearGradientButton'
 import LinearBorderGradientButton from '../../../CommonComponents/LinearBorderGradientButton'
 
 class Quiz extends Component {
   constructor (props) {
     super(props);
-    this.state = { responses:[] };
+    this.state = { responses: [] };
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.props.fetchQuestions()
   }
 
@@ -22,9 +22,9 @@ class Quiz extends Component {
     header: null
   };
 
-  questionAnswered=(answer) => {
-    this.setState({ responses: [...this.state.responses, answer]}, () => {
-      if(this.state.responses.length === this.props.questions.length){
+  questionAnswered = (answer) => {
+    this.setState({ responses: [...this.state.responses, answer] }, () => {
+      if (this.state.responses.length === this.props.questions.length) {
         this.props.navigation.navigate('Results', {
           responses: this.state.responses
         })
@@ -32,22 +32,26 @@ class Quiz extends Component {
     })
   };
 
-  render() {
+  render () {
+    if (this.props.isLoading) return <Loading />;
     return (
       <ImageBackground source={require('../../images/background_2.png')} style={styles.container}>
-        {
-          this.props.isLoading ? <Loading /> :
-            <SafeAreaView style={styles.container}>
-              <View style={{flex: 4, justifyContent:'center'}}>
-                <CardsList questions={this.props.questions} responses={this.state.responses}/>
-              </View>
+        <SafeAreaView style={styles.container}>
+          <View style={{ flex: 4, justifyContent: 'center' }}>
+            <CardsList questions={this.props.questions} responses={this.state.responses} />
+          </View>
 
-              <View style={{flexDirection:'row', flex: 1, marginHorizontal: 30}}>
-                <LinearBorderGradientButton containerStyle={{width:'50%', marginRight:5}} btnText={'FALSE'} onPress={()=>this.questionAnswered('False')} />
-                <LinearGradientButton containerStyle={{width:'50%'}} btnText={'TRUE'} onPress={()=>this.questionAnswered('True')} />
-              </View>
-            </SafeAreaView>
-        }
+          <View style={{ flexDirection: 'row', flex: 1, marginHorizontal: 30 }}>
+            <LinearBorderGradientButton
+              containerStyle={{ width: '50%', marginRight: 5 }}
+              btnText={'FALSE'}
+              onPress={() => this.questionAnswered('False')} />
+            <LinearGradientButton
+              containerStyle={{ width: '50%' }}
+              btnText={'TRUE'}
+              onPress={() => this.questionAnswered('True')} />
+          </View>
+        </SafeAreaView>
       </ImageBackground>
     )
   }
@@ -56,12 +60,12 @@ class Quiz extends Component {
 const styles = {
   container: {
     flex: 1,
-    justifyContent:'center',
-    alignItems:'center'
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 };
 
-const mapStateToProps = ({questions}) => {
+const mapStateToProps = ({ questions }) => {
   return {
     questions: questions.data,
     isLoading: questions.isLoading,
@@ -69,4 +73,4 @@ const mapStateToProps = ({questions}) => {
   }
 };
 
-export default connect(mapStateToProps, {fetchQuestions})(Quiz)
+export default connect(mapStateToProps, { fetchQuestions })(Quiz)
